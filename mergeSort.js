8665fef2c8041e83073eceab61c60cf07cadcb42
andre <123985988+derre3@users.noreply.github.com>
2023-10-30 14:42:30 -0300

    done mergeSort function

--- mergeSort.js
+++ mergeSort.js
@@ -1,14 +1,33 @@
 function mergeSort(arr) {
-  if (arr.length / 2 < 2) return arr;
-  else {
-    let newArr = arr.splice(0, arr.length / 2);
-    sort(arr);
-    sort(newArr);
-    arr = [newArr, arr];
-    return mergeSort(arr);
-  }
+  if (arr.length < 2) return arr;
+  const leftArr = arr.splice(0, arr.length / 2);
+  return merge(mergeSort(leftArr), mergeSort(arr));
 }
 
-function sort(arr) {}
+function merge(leftArr, rightArr) {
+  let merged = [];
+  let i = 0;
+  let j = 0;
 
-console.log(mergeSort([2, 8, 6, 1, 7]));
+  while (leftArr.length > i && rightArr.length > j) {
+    if (leftArr[i] < rightArr[j]) {
+      merged.push(leftArr[i]);
+      i++;
+    } else {
+      merged.push(rightArr[j]);
+      j++;
+    }
+  }
+
+  while (i < leftArr.length) {
+    merged.push(leftArr[i]);
+    i++;
+  }
+  while (j < rightArr.length) {
+    merged.push(rightArr[j]);
+    j++;
+  }
+  return merged;
+}
+
+console.log(mergeSort([8, 2, 6, 5, 7, 1, 4, 3]));
